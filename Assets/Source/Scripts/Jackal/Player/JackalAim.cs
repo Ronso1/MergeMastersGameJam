56909b7834aadf263bf97503e1600ea1676c3096
Сергей <2006lsd2006@gmail.com>
2023-09-15 16:06:42 +0300

    Add Shooting

---
+++ Assets/Source/Scripts/Jackal/Player/JackalAim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackalAim : MonoBehaviour
+{
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 diff = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Vector2.SignedAngle(Vector2.up, diff);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ Assets/Source/Scripts/Pool/Poolable.cs
@@ -0,0 +1,4 @@
+﻿public interface Poolable
+{
+    public void Reset();
+}
