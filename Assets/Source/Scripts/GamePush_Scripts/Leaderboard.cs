ddb074c120fa49d8402a351f3d910b2e8f4cbc15
Ivan <gureff.al@yandex.ru>
2023-09-30 14:18:26 +0300

    Improve leaderboard logic

--- Assets/Source/Scripts/GamePush_Scripts/Leaderboard.cs
+++ Assets/Source/Scripts/GamePush_Scripts/Leaderboard.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using GamePush;
+using TMPro;
 
 public class Leaderboard : MonoBehaviour
 {
     [SerializeField] public CalculateDistance score;
+    [SerializeField] private TMP_Text _showScore; 
     public void ShowLeaderboard()
     {
-        GP_Player.Set("score", score.playerScoreInt);
+        if ((int)GP_Player.GetScore() < score.playerScoreInt)
+        {
+            GP_Player.SetScore(score.playerScoreInt);
+            GP_Player.Sync(true);           
+        }
+        GP_Leaderboard.FetchPlayerRating();
         print(score.playerScoreInt);
-        GP_Leaderboard.Open();
+        GP_Leaderboard.Open("score");
     }
+
 }
 
