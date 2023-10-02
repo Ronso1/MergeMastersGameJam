34fe650858081801c0799c7efc2b683c7a918464
Ivan <gureff.al@yandex.ru>
2023-10-02 23:06:47 +0300

    Add cycle menu

--- Assets/Source/Scripts/Menu/CarMove.cs
+++ Assets/Source/Scripts/Menu/CarMove.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class CarMove : MonoBehaviour
 {
     [SerializeField] private Transform _car;
-    private Transform _newPoint;
-    private float _speed = 2f;
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private GameObject _firstLoop;
+
+    private float _positionToChangeChunk = 32f;
+    private float _positionToChange = 65f;
+    public List<GameObject> _chunks;
+    private int _count = 0;
+
 
     private void Start()
     {
-        _newPoint = _car.transform;
+        _chunks.Add(_firstLoop);
     }
 
     private void Update()
     {
-        _car.Translate(0f, -(_car.position.x - _speed - Time.time) * Time.deltaTime, 0f);
-
-
+        _car.Translate(0f, -(_car.position.x - _speed) * Time.deltaTime, 0f);
+        _speed += 5f * Time.deltaTime;
+        if (_car.transform.position.x >= _positionToChangeChunk)
+        {
+                                 
+            var chunk = Instantiate(_firstLoop);
+            chunk.transform.position = new Vector3(_positionToChange, 0f, 0f);
+            _chunks.Add(chunk);                       
+            _positionToChange += 65f;
+            _count++;
+            _chunks[_count].SetActive(true);
+            _positionToChangeChunk += 32f;
+        }
+       
     }
 
 }
