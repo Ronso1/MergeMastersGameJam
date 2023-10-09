c47a025eb3d9676fa835678910538268e464bb40
Сергей <2006lsd2006@gmail.com>
2023-10-09 17:44:45 +0300

    Add something in Root

--- Assets/Source/Scripts/Jackal/GameRoot.cs
+++ Assets/Source/Scripts/Jackal/GameRoot.cs
@@ -6,12 +6,17 @@ public class GameRoot : MonoBehaviour
 {
     [SerializeField] private LevelDigenerator _digenerator;
     [SerializeField] private EnemiesController _enemiesController;
-    
+
+    [SerializeField] private Joystick _joystick;
+
     private void Start()
     {
         _enemiesController.Init();
         _digenerator.Init();
 
-        
+        if (GamePush.GP_Device.IsMobile())
+        {
+            _joystick.enabled = true;
+        }
     }
 }
